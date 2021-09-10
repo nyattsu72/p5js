ad29aa76930f21d4b82011fe132b6f044ac98a92
nyattsu <seki72ko@gmail.com>
2021-09-10 15:53:41 +0900

    add:lesson01 p5.js

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
+let locationX = 0;
+let velocityX = 30;
+
 function setup() {
-	frameRate(1);
+	frameRate(10);
 	createCanvas(500, 300);
+	colorMode(HSB);
 }
 
 function draw() {
-	ellipse(50, 50, 80, 80);
+	// background('#fff');
+	noStroke();
+	fill(random(160, 200), 100, 100, 0.3);
+	ellipse(random(0, width), random(0, height), 80, 80);
+
+	// let randomTest = random(180, 10);
+	// console.log(randomTest);
+
+	locationX += velocityX;
+	// console.log(locationX);
+
+	if (locationX > 400) {
+		locationX = 0;
+	}
 }
